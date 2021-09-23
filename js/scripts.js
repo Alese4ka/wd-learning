ef4a630255929792378d71df4573e7396132338e
Alese4ka <alesun4ik@gmail.com>
2021-09-23 14:29:37 +0300

    feature/CT-5: -create function blockHide()

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const arrowUp = document.querySelector('.sixth-block__form-services__arrow');
+const servicesList = document.querySelector('.sixth-block__form-services__list');
+
+arrowUp.addEventListener('click', () => blockHide());
+
+function blockHide () {
+  if(servicesList.style.display == `none`) {
+    servicesList.style.display = `flex`;
+  }
+  else {
+    servicesList.style.display = `none`;
+  }
+}
