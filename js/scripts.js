bb4b6d117d349fa28cfcfc9ded5eee5ee4259d34
Alese4ka <alesun4ik@gmail.com>
2021-09-26 16:34:30 +0300

    feature/CT-5: -create functions changeSlide()

--- js/scripts.js
+++ js/scripts.js
@@ -5,11 +5,16 @@ const arrowRight = document.querySelector('.fourth-block__cars__description__arr
 const carOne = document.querySelector('.fourth-block__cars__description-one')
 const carTwo = document.querySelector('.fourth-block__cars__description-two')
 const carThree = document.querySelector('.fourth-block__cars__description-three')
+const carBtnOne = document.querySelector('.fourth-block__cars-button-one');
+const carBtnTwo = document.querySelector('.fourth-block__cars-button-two');
+const carBtnThree = document.querySelector('.fourth-block__cars-button-three');
 
 arrowUp.addEventListener('click', () => blockHide());
 arrowLeft.addEventListener('click', () => changeSlideLeft());
 arrowRight.addEventListener('click', () => changeSlideRight());
-
+carBtnOne.addEventListener('click', () => changeSlideOne());
+carBtnTwo.addEventListener('click', () => changeSlideTwo());
+carBtnThree.addEventListener('click', () => changeSlideThree());
 
 function blockHide () {
   if(servicesList.style.display == `none`) {
@@ -49,3 +54,28 @@ function changeSlideRight () {
     carOne.style.display = `flex`;
   }
 }
+
+function changeSlideOne () {
+  if(carTwo.style.display == `flex` || carThree.style.display == `flex`){
+    carOne.style.display = `flex`;
+    carTwo.style.display = `none`;
+    carThree.style.display = `none`;
+  }
+}  
+
+function changeSlideTwo () {
+  if(carOne.style.display == `flex` || carThree.style.display == `flex`){
+    carTwo.style.display = `flex`;
+    carOne.style.display = `none`;
+    carThree.style.display = `none`;
+  }
+}  
+
+function changeSlideThree () {
+  if(carTwo.style.display == `flex` || carOne.style.display == `flex`){
+    carThree.style.display = `flex`;
+    carTwo.style.display = `none`;
+    carOne.style.display = `none`;
+  }
+}  
+
