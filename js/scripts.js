be4c045983226869ad123b27765827fc070c4c03
Alese4ka <alesun4ik@gmail.com>
2021-09-27 13:28:20 +0300

    feature/CT-5: -create functions startTimer() and validation form

--- js/scripts.js
+++ js/scripts.js
@@ -79,3 +79,51 @@ function changeSlideThree () {
   }
 }  
 
+/*Timer*/
+function startTimer() { 
+  let countDownDate = new Date();
+  countDownDate.setHours(countDownDate.getHours() + 6);
+  
+  let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timer").innerHTML = hours + ":" + minutes + ":" + seconds;
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("timer").innerHTML = "Время вышло";
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  startTimer();
+};
+
+/*Validation form*/
+const form = document.querySelector('.sixth-block__form-order');
+const submitBtn = form.querySelector('.sixth-block__form-time__main-button');
+const login = form.querySelector('.sixth-block__form-contact__name');
+const phone = form.querySelector('.sixth-block__form-contact__tel');
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+
+    if (!login.value) {
+      login.style.border = '1px solid #ff352b';
+    }
+    if (!phone.value) {
+      phone.style.border = '1px solid #ff352b';
+    }
+    if (login.value) {
+      login.style.border = '1px solid green';
+    }
+    if (phone.value) {
+      phone.style.border = '1px solid green';
+    }
+})
