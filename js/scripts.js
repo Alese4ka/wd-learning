7689b2ee9884b4db8775186c443c3649d2aca387
Alese4ka <alesun4ik@gmail.com>
2021-09-28 12:00:53 +0300

    feature/CT-5: -create function burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -180,12 +180,18 @@ Share = {
 };
 
 /*Burger menu*/
-const burger = document.querySelector('.header-block__hamburger-menu');
-const firstBlock = document.querySelector('.first-block');
+const burgerBtn = document.querySelector('.header-block__hamburger-menu__button');
+const burgerLinesMenu = document.querySelector('.header-block__hamburger-menu__lines');
+const burgerMenu = document.querySelector('.header-block__hamburger-main-menu');
+const burgerMenuOverlay = document.querySelector('.header-block__hamburger-menu__overlay');
 
-burger.addEventListener('click', function(){
-  document.querySelector('.header-block__hamburger-menu span').classList.toggle('active');
-  document.querySelector('.header-block__hamburger-main-menu').classList.toggle('animate');
-})
-
+burgerBtn.addEventListener('click', () => menuShowHide());
+burgerMenu.addEventListener('click', () => menuShowHide());
+burgerMenuOverlay.addEventListener('click', () => menuShowHide());
+  
+function menuShowHide() {
+  burgerLinesMenu.classList.toggle('active');
+  burgerMenu.classList.toggle('animate');
+  burgerMenuOverlay.classList.toggle('animate');
+}
 
