da985ed598895916c28cd6ebab63a1a14e1dff35
Alese4ka <alesun4ik@gmail.com>
2021-10-01 15:28:32 +0300

    feature/CT-5: -correct mistakes

--- js/hide-block.js
+++ js/hide-block.js
@@ -1,14 +1,19 @@
 /*Hide block*/
 const arrowUp = document.querySelector('.sixth-block__form-services__arrow');
 const servicesList = document.querySelector('.sixth-block__form-services__list');
+const arrowBlock = document.querySelector('.sixth-block__form-services-p');
 
 arrowUp.addEventListener('click', () => blockHide());
 
 function blockHide() {
   if(servicesList.style.display == 'none') {
     servicesList.style.display = 'flex';
+    arrowBlock.classList.toggle('hide-block');
+    arrowUp.classList.toggle('hide-block-arrow');
   }
   else {
     servicesList.style.display = 'none';
+    arrowBlock.classList.toggle('hide-block');
+    arrowUp.classList.toggle('hide-block-arrow');
   }
 }
