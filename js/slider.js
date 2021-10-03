8161f7eb66b1a0d7fbdd164a055d234f6b29b1b3
Alese4ka <alesun4ik@gmail.com>
2021-10-03 21:16:08 +0300

    feature/CT-5: -correct mistakes

--- js/slider.js
+++ js/slider.js
@@ -10,36 +10,6 @@ const carBtnThree = document.querySelector('.fourth-block__cars-button-three');
 
 arrowLeft.addEventListener('click', () => previousSlide());
 arrowRight.addEventListener('click', () => nextSlide());
-carBtnOne.addEventListener('click', () => changeSlides('one'));
-carBtnTwo.addEventListener('click', () => changeSlides('two'));
-carBtnThree.addEventListener('click', () => changeSlides('three'));
-
-function changeSlides(btn) {
-  if(btn === 'one'){
-    carOne.style.display = 'flex';
-    carTwo.style.display = 'none';
-    carThree.style.display = 'none';
-    carBtnTwo.style.border = '0.0625rem solid #ebebeb';
-    carBtnThree.style.border = '0.0625rem solid #ebebeb';
-    carBtnOne.style.border = '0.0625rem solid #fed100';
-  }
-  else if(btn === 'two'){
-    carTwo.style.display = 'flex';
-    carOne.style.display = 'none';
-    carThree.style.display = 'none';  
-    carBtnOne.style.border = '0.0625rem solid #ebebeb';
-    carBtnThree.style.border = '0.0625rem solid #ebebeb';
-    carBtnTwo.style.border = '0.0625rem solid #fed100';
-  }
-  else if(btn === 'three'){
-    carThree.style.display = 'flex';
-    carTwo.style.display = 'none';
-    carOne.style.display = 'none';
-    carBtnTwo.style.border = '0.0625rem solid #ebebeb';
-    carBtnOne.style.border = '0.0625rem solid #ebebeb';
-    carBtnThree.style.border = '0.0625rem solid #fed100';
-  }
-} 
 
 let slideIndex = 1;
 showSlides(slideIndex);
@@ -74,4 +44,64 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = 'flex';
   btnSlides[slideIndex - 1].className += ' color-border';
+}
+
+let mediaQuery = window.matchMedia('(min-width: 1211px)');
+carBtnOne.addEventListener('click', () => changeSlides('one'));
+carBtnTwo.addEventListener('click', () => changeSlides('two'));
+carBtnThree.addEventListener('click', () => changeSlides('three'));
+
+function changeSlides(btn) {
+  if (mediaQuery.matches) {
+    if(btn === 'one'){
+      carOne.style.display = 'flex';
+      carTwo.style.display = 'none';
+      carThree.style.display = 'none';
+      carBtnTwo.style.border = '0.0625rem solid #ebebeb';
+      carBtnThree.style.border = '0.0625rem solid #ebebeb';
+      carBtnOne.style.border = '0.0625rem solid #fed100';
+    }
+    else if(btn === 'two'){
+      carTwo.style.display = 'flex';
+      carOne.style.display = 'none';
+      carThree.style.display = 'none';  
+      carBtnOne.style.border = '0.0625rem solid #ebebeb';
+      carBtnThree.style.border = '0.0625rem solid #ebebeb';
+      carBtnTwo.style.border = '0.0625rem solid #fed100';
+    }
+    else if(btn === 'three'){
+      carThree.style.display = 'flex';
+      carTwo.style.display = 'none';
+      carOne.style.display = 'none';
+      carBtnTwo.style.border = '0.0625rem solid #ebebeb';
+      carBtnOne.style.border = '0.0625rem solid #ebebeb';
+      carBtnThree.style.border = '0.0625rem solid #fed100';
+    }  
+  }
+  else {
+    if(btn === 'one'){
+      carOne.style.display = 'flex';
+      carTwo.style.display = 'none';
+      carThree.style.display = 'none';
+      carBtnTwo.style.border = 'none';
+      carBtnThree.style.border = 'none';
+      carBtnOne.style.border = 'none';
+    }
+    else if(btn === 'two'){
+      carTwo.style.display = 'flex';
+      carOne.style.display = 'none';
+      carThree.style.display = 'none';  
+      carBtnOne.style.border = '0.0625rem solid none';
+      carBtnThree.style.border = '0.0625rem solid none';
+      carBtnTwo.style.border = '0.0625rem solid none';
+    }
+    else if(btn === 'three'){
+      carThree.style.display = 'flex';
+      carTwo.style.display = 'none';
+      carOne.style.display = 'none';
+      carBtnTwo.style.border = '0.0625rem solid none';
+      carBtnOne.style.border = '0.0625rem solid none';
+      carBtnThree.style.border = '0.0625rem solid none';
+    }
+  }
 }
